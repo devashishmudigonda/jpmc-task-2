2cb4136629fd7881e74576f3c0a7d79faa5d380d
devashishmudigonda <devashish_mudigonda@yahoo.com>
2023-02-19 10:03:34 +0530

    TASK-2

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,20 @@ import React, { Component } from 'react';
 import DataStreamer, { ServerRespond } from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
-
 /**
  * State declaration for <App />
  */
 interface IState {
   data: ServerRespond[],
+  showGraph: boolean,
 }
 
 /**
- * The parent element of the react app.
- * It renders title, button and Graph react element.
- */
-class App extends Component<{}, IState> {
-  constructor(props: {}) {
-    super(props);
-
-    this.state = {
+@@ -22,25 +23,38 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false,
     };
   }
 
@@ -29,7 +23,9 @@ class App extends Component<{}, IState> {
    * Render Graph react component with state.data parse as property data
    */
   renderGraph() {
+    if(this.state.showGraph) {
     return (<Graph data={this.state.data}/>)
+    }
   }
 
   /**
@@ -37,10 +33,23 @@ class App extends Component<{}, IState> {
    */
   getDataFromServer() {
     DataStreamer.getData((serverResponds: ServerRespond[]) => {
+    let x = 0;
+    const interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
       // Update the state by creating a new array of data that consists of
       // Previous data in the state and the new data from server
       this.setState({ data: [...this.state.data, ...serverResponds] });
     });
+        this.setState({
+          data: serverResponds,
+          showGraph: true,
+        });
+      });
+      x++;
+      if (x > 1000) {
+        clearInterval(interval);
+      }
+    }, 100);  
   }
 
   /**
@@ -70,5 +79,4 @@ class App extends Component<{}, IState> {
     )
   }
 }
-
 export default App;
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -2,53 +2,43 @@ import React, { Component } from 'react';
 import { Table } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
 import './Graph.css';
-
 /**
  * Props declaration for <Graph />
  */
 interface IProps {
   data: ServerRespond[],
 }
-
 /**
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
 interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
-/**
- * React component that renders Perspective based on data
- * parsed from its parent through data property.
- */
-class Graph extends Component<IProps, {}> {
-  // Perspective table
-  table: Table | undefined;
-
-  render() {
-    return React.createElement('perspective-viewer');
-  }
+@@ -32,7 +32,7 @@ class Graph extends Component<IProps, {}> {
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
     const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
       stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
-    };
-
-    if (window.perspective && window.perspective.worker()) {
-      this.table = window.perspective.worker().table(schema);
-    }
-    if (this.table) {
-      // Load the `table` in the `<perspective-viewer>` DOM reference.
+@@ -49,6 +49,15 @@ class Graph extends Component<IProps, {}> {
 
       // Add more Perspective configurations here.
       elem.load(this.table);
+      elem.setAttribute('view', 'y_line');
+      elem.setAttribute('column-pivots', '["stock"]');
+      elem.setAttribute('row-pivots', '["timestamp"]');
+      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('aggregates', `
+        {"stock": "distinct_count",
+        "top_ask_price": "avg",
+        "top_bid_price": "avg",
+        "timestamp": "distinct_count"}`);
     }
   }
 
@@ -69,5 +59,4 @@ class Graph extends Component<IProps, {}> {
     }
   }
 }
-
 export default Graph;
